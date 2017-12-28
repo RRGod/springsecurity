776b4b85d1fe91848b008dea543b210afb6a6cf0
xuan.hou <xuan.hou@yidatec.com>
2017-12-28 16:36:07 +0800

    Initial commit

---
+++ src/main/java/com/nextinnovation/springsecurity/tool/UrlMatcher.java
@@ -0,0 +1,8 @@
+package com.nextinnovation.springsecurity.tool;
+
+public interface UrlMatcher{
+    Object compile(String paramString);
+    boolean pathMatchesUrl(Object paramObject, String paramString);
+    String getUniversalMatchPattern();
+    boolean requiresLowerCaseUrl();
+}
